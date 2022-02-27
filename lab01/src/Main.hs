f75686984d3897187a4b886719f2fa3b221a336d
Dmitry Kalinin <x.shreddered.x@gmail.com>
2022-02-27 21:11:14 +0300

    added base types

--- lab01/src/Main.hs
+++ lab01/src/Main.hs
@@ -1,9 +1,23 @@
 module Main where
 
-data ActivationFunction = ActivationFunction { f :: Double -> Bool
+import Data.Vector
+
+type Matrix a = Vector (Vector a)
+
+data ActivationFunction = ActivationFunction { f :: Double -> Double
                                              , f' :: Double -> Double
                                              }
 
+data Layer = Layer { weights            :: Matrix Double
+                   , activationFunction :: ActivationFunction
+                   }
+
+data NeuralNetwork = NeuralNetwork { layers       :: [Layer]
+                                   , learningRate :: Double
+                                   }
+
+train :: NeuralNetwork -> Vector Double -> NeuralNetwork
+train neuralNetwork = _a
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
