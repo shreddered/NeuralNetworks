e95fb173f6816a0b7642c61c68679b79d13453c4
Dmitry Kalinin <x.shreddered.x@gmail.com>
2022-03-05 19:16:04 +0300

    added basic data types

--- lab01/src/Neuron.hs
+++ lab01/src/Neuron.hs
@@ -1,6 +1,20 @@
 module Neuron
-    ( someFunc
+    ( ActivationFunction
+    , Neuron
+    , someFunc
     ) where
+
+import Numeric.LinearAlgebra.HMatrix
+
+-- for now I will keep it simple
+data ActivationFunction = ActivationFunction { f  :: Double -> Double
+                                             , f' :: Double -> Double
+                                             }
+
+data Neuron = Neuron { learningRate :: Double
+                     , activationFunction :: ActivationFunction
+                     , weights :: Matrix Double
+                     }
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
