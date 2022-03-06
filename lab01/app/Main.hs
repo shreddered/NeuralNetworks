4d4a8e2ccfd7fb4a644f89f43b8a1e89da42e896
Dmitry Kalinin <x.shreddered.x@gmail.com>
2022-03-06 16:56:56 +0300

    added plots

--- lab01/app/Main.hs
+++ lab01/app/Main.hs
@@ -2,6 +2,9 @@ module Main where
 
 import Data.Neuron
 
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Cairo
+
 main :: IO ()
 main = do
   let thresholdAF = ActivationFunction
@@ -13,5 +16,13 @@ main = do
           , derivative = (/ 2) . (^ (-2)) . cosh
           }
       func = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1 ]
-      weights = train func 0.3 thresholdAF
+      (weights, errors) = train func 0.3 thresholdAF
+      thresholdPlot = zip [1..] errors
   print weights
+  toFile def "images/plot1.png" $ do
+    layout_title .= "Пороговая функция активации"
+    layout_x_axis . laxis_title .= "k"
+    layout_y_axis . laxis_title .= "E(k)"
+    setColors [opaque blue, opaque red]
+    plot (line "Количество ошибок за эпоху" [thresholdPlot])
+    plot (points "Количество ошибок за эпоху" (thresholdPlot))
