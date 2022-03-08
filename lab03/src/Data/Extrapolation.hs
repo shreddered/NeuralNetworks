6164c3860367f7921e0a836080157d6557a034cc
Dmitry Kalinin <x.shreddered.x@gmail.com>
2022-03-08 23:42:56 +0300

    added some functions

--- lab03/src/Data/Extrapolation.hs
+++ lab03/src/Data/Extrapolation.hs
@@ -1,6 +1,17 @@
 module Data.Extrapolation
-    ( someFunc
+    ( ActivationFunction (..)
+    , generatePoints
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data ActivationFunction = ActivationFunction
+    { primary    :: Double -> Double
+    , derivative :: Double -> Double
+    }
+
+-- function for point generation
+generatePoints :: Double   -- a
+               -> Double   -- b
+               -> Int      -- number of points
+               -> [Double] -- points
+generatePoints a b n = let step = (b - a) / (realToFrac n)
+                        in take n $ iterate (+ step) a
