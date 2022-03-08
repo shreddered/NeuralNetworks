b3f6ace254e268fbc047f0f53539d447cd8eea6e
Dmitry Kalinin <x.shreddered.x@gmail.com>
2022-03-09 00:08:33 +0300

    draft train implementation

--- lab03/src/Data/Extrapolation.hs
+++ lab03/src/Data/Extrapolation.hs
@@ -9,9 +9,19 @@ data ActivationFunction = ActivationFunction
     }
 
 -- function for point generation
-generatePoints :: Double   -- a
-               -> Double   -- b
-               -> Int      -- number of points
-               -> [Double] -- points
-generatePoints a b n = let step = (b - a) / (realToFrac n)
-                        in take n $ iterate (+ step) a
+generatePoints :: (Double, Double) -- (a, b)
+               -> Int              -- number of points
+               -> [Double]         -- points
+generatePoints (a, b) n = let step = (b - a) / (realToFrac n)
+                           in take n $ iterate (+ step) a
+
+train :: Double             -- learning rate
+      -> [Double]           -- points
+      -> Int                -- number of epochs
+      -> Int                -- size of window
+      -> (Double, [Double]) -- (epsilon, weights)
+train learningRate points n windowSize =
+    foldl foo (0, replicate (windowSize + 1) 0) [0..fromIntegral $ n - 1]
+  where
+    foo (eps, weights) k = let xs = take windowSize $ drop k points
+                            in _a
