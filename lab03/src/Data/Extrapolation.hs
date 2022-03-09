412806f8697295ba6ba4ad549c8f10a9ace53ff0
Dmitry Kalinin <x.shreddered.x@gmail.com>
2022-03-09 22:39:40 +0300

    implemented train procedure

--- lab03/src/Data/Extrapolation.hs
+++ lab03/src/Data/Extrapolation.hs
@@ -1,12 +1,9 @@
 module Data.Extrapolation
-    ( ActivationFunction (..)
-    , generatePoints
+    ( generatePoints
+    , train
     ) where
 
-data ActivationFunction = ActivationFunction
-    { primary    :: Double -> Double
-    , derivative :: Double -> Double
-    }
+import Control.Arrow
 
 -- function for point generation
 generatePoints :: (Double, Double) -- (a, b)
@@ -15,13 +12,17 @@ generatePoints :: (Double, Double) -- (a, b)
 generatePoints (a, b) n = let step = (b - a) / (realToFrac n)
                            in take n $ iterate (+ step) a
 
-train :: Double             -- learning rate
-      -> [Double]           -- points
-      -> Int                -- number of epochs
-      -> Int                -- size of window
-      -> (Double, [Double]) -- (epsilon, weights)
+train :: Double               -- learning rate
+      -> [Double]             -- points
+      -> Int                  -- number of epochs
+      -> Int                  -- size of window
+      -> ([Double], [Double]) -- (epsilon, weights)
 train learningRate points n windowSize =
-    foldl foo (0, replicate (windowSize + 1) 0) [0..fromIntegral $ n - 1]
+    foldl something ([], replicate (windowSize + 1) 0) $ replicate n [0..18-windowSize]
   where
-    foo (eps, weights) k = let xs = take windowSize $ drop k points
-                            in _a
+    something x = epoch x . map (\k -> (take windowSize $ drop k points, points !! (k + windowSize + 1)))
+    epoch x@(es, _) = first (: es) . first sqrt . foldl foo (0, snd x)
+    foo params@(_, weights) (column, x) = let net = sum (zipWith (*) column (tail weights)) + (head weights)
+                                              delta = x - net
+                                              newWeights = zipWith (\xn w -> w + learningRate * delta * xn) (1:column)
+                                           in ((+ delta^2) *** newWeights) params
