13cb4cf0da438ee0702d41fc4a06be35df30fa95
Dmitry Kalinin <x.shreddered.x@gmail.com>
2022-03-10 09:51:26 +0300

    code style edits

--- lab03/src/Data/Extrapolation.hs
+++ lab03/src/Data/Extrapolation.hs
@@ -22,7 +22,7 @@ train learningRate points n windowSize =
     foldl something ([], replicate (windowSize + 1) 0) $ replicate n [0..18-windowSize]
   where
     something x = epoch x . map (\k -> (take windowSize $ drop k points, points !! (k + windowSize + 1)))
-    epoch x@(es, _) = first (: es) . first sqrt . foldl foo (0, snd x)
+    epoch (es, weights) = first (: es) . first sqrt . foldl foo (0, weights)
     foo params@(_, weights) (column, x) = let net = sum (zipWith (*) column (tail weights)) + (head weights)
                                               delta = x - net
                                               newWeights = zipWith (\xn w -> w + learningRate * delta * xn) (1:column)
