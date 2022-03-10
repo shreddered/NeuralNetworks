09bc370610a0d7ec64c177cd001deccc2577afac
Dmitry Kalinin <x.shreddered.x@gmail.com>
2022-03-10 09:29:08 +0300

    new plots

--- lab03/app/Main.hs
+++ lab03/app/Main.hs
@@ -25,11 +25,11 @@ main :: IO ()
 main = do
   let ts = generatePoints' (5, 9) 20
       points = func <$> generatePoints (1, 5) 20
-      (eps, weights) = train 0.1 points 60000 6
+      (eps, weights) = train 0.1 points 2000 6
       (x1, x2) = getPredictions points 6 weights
   putStrLn $ intercalate ", " (printf "%.4f" <$> weights)
   putStrLn $ intercalate ", " (printf "%.4f" <$> (x1 ++ x2))
-  toFile def "images/plot1.png" $ do
+  toFile def "images/plot2.png" $ do
     layout_title .= "График исходной функции на интервале [a, 2b-a]"
     layout_x_axis . laxis_title .= "t"
     layout_y_axis . laxis_title .= "x"
