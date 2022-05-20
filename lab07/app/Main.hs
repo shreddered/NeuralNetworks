7071a11659ff64be50c7e1e911b93c95631bcf92
Dmitry Kalinin <x.shreddered.x@gmail.com>
2022-05-20 03:17:29 +0300

    added simple main

--- lab07/app/Main.hs
+++ lab07/app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Numeric.Neural.Hopfield
+import Numeric.LinearAlgebra
 
 main :: IO ()
 main = do
@@ -32,12 +33,44 @@ main = do
       network = initHopfield vectors
   putStrLn "Weights after initialization:"
   print $ weights network
-  let six'  = [  1,  1,  1, -1
-              ,  1, -1,  1, -1
-              ,  1, -1, -1, -1
-              ,  1,  1,  1,  1
-              ,  1, -1, -1,  1
-              ,  1, -1, -1,  1
-              ,  1,  1,  1,  1
-              ]
-  print $ evalHopfield network six'
+  putStrLn "Check original images:"
+  putStrLn "6:"
+  print $ matrix 4 $ evalHopfield network six
+  putStrLn "4:"
+  print $ matrix 4 $ evalHopfield network four
+  putStrLn "7:"
+  print $ matrix 4 $ evalHopfield network seven
+  let six'   = [  1,  1,  1, -1
+               ,  1, -1,  1, -1
+               ,  1, -1, -1, -1
+               ,  1,  1,  1,  1
+               ,  1, -1, -1,  1
+               ,  1, -1, -1,  1
+               ,  1,  1,  1,  1
+               ]
+      seven' = [  1,  1,  1,  1
+               ,  1, -1, -1,  1
+               , -1, -1, -1,  1
+               , -1, -1, -1,  1
+               , -1, -1, -1,  1
+               , -1, -1, -1,  1
+               , -1, -1, -1,  1
+               ]
+      four'  = [  1, -1, -1,  1
+               ,  1, -1, -1,  1
+               ,  1,  1, -1,  1
+               ,  1, -1,  1,  1
+               , -1, -1, -1,  1
+               , -1, -1, -1,  1
+               , -1, -1, -1,  1
+               ]
+      six'' = evalHopfield network six'
+      seven'' = evalHopfield network seven'
+      four'' = evalHopfield network four'
+  putStrLn "After corruption:"
+  putStrLn "6:"
+  print $ matrix 4 six''
+  putStrLn "7:"
+  print $ matrix 4 seven''
+  putStrLn "4:"
+  print $ matrix 4 four''
